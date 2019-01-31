daf0f5950d294893bdb2111577f353fb5ced02d6
Ryan <rcphillips155@gmail.com>
2019-01-31 17:53:04 -0600

    update celestials

--- PythonWrapper/celestialobjects.py
+++ PythonWrapper/celestialobjects.py
@@ -8,3 +8,26 @@ class Planet(object):
 	def __init__(self, radius, s_pos, material = None, color = None):
 
 		pass
+
+
+
+
+class Star(object):
+
+	def __init__(self, radius, s_pos, color = vpython.colors.yellow):
+
+
+		pass
+
+
+
+
+class Comet(object):
+
+	def __init__(self, close_approach_object):
+
+
+		self.data = close_approach_object.get_frame()
+
+		pass
+		
